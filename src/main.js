82f03808fe9a796d19ef3b8cf9ed18eb8736fdc0
Wang JR <1009852179@qq.com>
2024-03-22 20:58:11 +0800

    吸顶导航交互实现

--- src/main.js
+++ src/main.js
@@ -8,12 +8,6 @@ import App from './App.vue'
 import router from './router'
 
 
-//测试接口函数
-import { getCategory } from '@/apis/textAPI'
-
-getCategory().then(res => {
-  console.log(res)
-})
 const app = createApp(App)
 
 app.use(createPinia())
--- src/views/Layout/index-i.vue
+++ src/views/Layout/index-i.vue
@@ -2,12 +2,14 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
 
 
 </script>
 
 <template>
   <div>
+    <LayoutFixed></LayoutFixed>
     <LayoutNav />
     <LayoutHeader />
     <!-- 二级路由出口 -->
